a24152e585cd6dd1fb35cdb427112caa50c82efd
alexKos58 <alex_kostyushin@mail.ru>
2023-05-16 16:01:17 +0300

    Add annotations @Transactional

--- src/main/java/ru/kostyushin/webapp/service/CategoryService.java
+++ src/main/java/ru/kostyushin/webapp/service/CategoryService.java
@@ -2,6 +2,7 @@ package ru.kostyushin.webapp.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import ru.kostyushin.webapp.entity.Category;
 import ru.kostyushin.webapp.repository.CategoryRepository;
 
@@ -17,18 +18,22 @@ public class CategoryService {
         this.categoryRepository = categoryRepository;
     }
 
+    @Transactional(readOnly=true)
     public Category findById(Long id) {
         return categoryRepository.findById(id).orElse(null);
     }
 
+    @Transactional(readOnly=true)
     public Iterable<Category> findAll() {
         return categoryRepository.findAll();
     }
 
+    @Transactional
     public Category saveCategory(Category category) {
         return categoryRepository.save(category);
     }
 
+    @Transactional
     public void deleteById(Long id) throws EntityNotFoundException {
         Category category = categoryRepository.findById(id).orElseThrow(
                 () -> new EntityNotFoundException("There is no such category!"));
--- src/main/java/ru/kostyushin/webapp/service/ProductService.java
+++ src/main/java/ru/kostyushin/webapp/service/ProductService.java
@@ -2,6 +2,7 @@ package ru.kostyushin.webapp.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import ru.kostyushin.webapp.entity.Product;
 import ru.kostyushin.webapp.repository.ProductRepository;
 
@@ -17,19 +18,22 @@ public class ProductService {
         this.productRepository = productRepository;
     }
 
+    @Transactional(readOnly=true)
     public Product findById(Long id) {
         return productRepository.findById(id).orElse(null);
     }
 
+    @Transactional(readOnly=true)
     public Iterable<Product> findAll() {
         return productRepository.findAll();
     }
 
-
+    @Transactional
     public Product saveProduct(Product product){
     return productRepository.save(product);
     }
 
+    @Transactional()
     public void deleteById(Long id) throws EntityNotFoundException {
         Product product = productRepository.findById(id).orElseThrow(
                 () -> new EntityNotFoundException("There is no such product!"));
