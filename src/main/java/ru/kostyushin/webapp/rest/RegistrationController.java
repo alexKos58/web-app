ffe3dd68291bc1fb101002a76532b69ab295573a
alexKos58 <alex_kostyushin@mail.ru>
2023-05-16 16:03:32 +0300

    Move Role class

--- src/main/java/ru/kostyushin/webapp/rest/RegistrationController.java
+++ src/main/java/ru/kostyushin/webapp/rest/RegistrationController.java
@@ -4,7 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import ru.kostyushin.webapp.entity.Role;
+import ru.kostyushin.webapp.entity.enums.Role;
 import ru.kostyushin.webapp.entity.User;
 import ru.kostyushin.webapp.repository.UserRepository;
 
